b7a122e4c7de84865e4e64fe4e41978ed355d691
SOPPA SUDEENDRA SRIPADA <soppasripada@gmail.com>
2026-02-03 04:11:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4967399350-28365",
+  "appId": "1:1093161793057:web:c616ad12030c3d6e5b9560",
+  "apiKey": "AIzaSyCAbhAVgQKxhOh6OM7sTtX0Amh-cbxifbk",
+  "authDomain": "studio-4967399350-28365.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1093161793057"
+};
