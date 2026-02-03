e3dac892e0ba98e1498644bcdb077ea9d039a7da
SOPPA SUDEENDRA SRIPADA <soppasripada@gmail.com>
2026-02-03 06:53:05 +0000

    make sure the image is addedinto database and add it correctly it is not

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -49,7 +49,7 @@ export function Header() {
                   width={40} 
                   height={40} 
                   data-ai-hint={logo.imageHint}
-                  className="h-10 w-auto object-contain"
+                  className="object-contain"
                 />
               </div>
             ) : (
@@ -117,3 +117,5 @@ export function Header() {
     </header>
   )
 }
+
+    
